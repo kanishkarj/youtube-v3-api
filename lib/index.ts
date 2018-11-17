fc631264930161b6efd2415354d144d3b740af79
kanishkarj <kanishkarj@hotmail.com>
2018-11-17 21:57:04 +0530

    Add Comments listing and Comments threads listing

--- lib/index.ts
+++ lib/index.ts
@@ -4,7 +4,9 @@ import {
     SEARCH_CHANNEL_URL, 
     SEARCH_PLAYLIST_URL,
     SEARCH_PLAYLIST_ITEMS_URL,
-    SEARCH_VIDEO_URL
+    SEARCH_VIDEO_URL,
+    SEARCH_COMMENT_THREADS_URL,
+    SEARCH_COMMENTS_URL
 } from './urls';
 
 export function search_all (
@@ -121,6 +123,48 @@ export function search_video (
         args.key = API_KEY;
 
         axios.get(SEARCH_VIDEO_URL,{
+            params : args
+        }).then((response:any) => {
+            resolve(response.data);
+        }).catch((err) => {
+            reject(new Error(err));
+        });
+    });    
+}
+
+export function search_comment_threads (
+    API_KEY : String, 
+    videoId : String,
+    args : any = {},        
+) {
+    return new Promise<{}>((resolve, reject) => {
+
+        args.part = "snippet,replies";
+        args.videoId = videoId;
+        args.key = API_KEY;
+
+        axios.get(SEARCH_COMMENT_THREADS_URL,{
+            params : args
+        }).then((response:any) => {
+            resolve(response.data);
+        }).catch((err) => {
+            reject(new Error(err));
+        });
+    });    
+}
+
+export function search_comments (
+    API_KEY : String, 
+    parentId : String,
+    args : any = {},        
+) {
+    return new Promise<{}>((resolve, reject) => {
+
+        args.part = "snippet,replies";
+        args.parentId = parentId;
+        args.key = API_KEY;
+
+        axios.get(SEARCH_COMMENTS_URL,{
             params : args
         }).then((response:any) => {
             resolve(response.data);
--- lib/urls.ts
+++ lib/urls.ts
@@ -3,3 +3,5 @@ export const SEARCH_CHANNEL_URL = "https://www.googleapis.com/youtube/v3/channel
 export const SEARCH_PLAYLIST_URL = "https://www.googleapis.com/youtube/v3/playlists";
 export const SEARCH_PLAYLIST_ITEMS_URL = "https://www.googleapis.com/youtube/v3/playlistItems";
 export const SEARCH_VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos";
+export const SEARCH_COMMENTS_URL = "https://www.googleapis.com/youtube/v3/comments";
+export const SEARCH_COMMENT_THREADS_URL = "https://www.googleapis.com/youtube/v3/commentThreads";
